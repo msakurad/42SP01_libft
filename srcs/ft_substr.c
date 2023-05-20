39d02a373c0ee82c4f549eadc2dbe824d92e630f
msakurad <danieldarthez@proton.me>
2023-05-20 09:59:45 -0300

    fix: tripouille, substr

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: msakurad <msakurad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:35:28 by msakurad          #+#    #+#             */
-/*   Updated: 2023/05/16 16:38:02 by msakurad         ###   ########.fr       */
+/*   Updated: 2023/05/20 09:58:53 by msakurad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 
 	s_len = ft_strlen(s);
-	if (!s || len >= __INT_MAX__ || (size_t)start > s_len)
+	if (!s || len >= __INT_MAX__)
 		return (NULL);
+	if ((size_t)start > s_len)
+		return ft_strdup("");
 	if (s_len - (size_t)start < len)
 		len = s_len - (size_t)start;
 	sub_s = (char *)malloc(sizeof(char) * (len + 1));
+	if (sub_s == NULL)
+		return (NULL);
 	ft_strlcpy(sub_s, &s[start], len + 1);
 	return (sub_s);
 }
