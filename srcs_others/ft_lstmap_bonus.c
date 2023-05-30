3febc3ab57bdcfdfb54d9e2faa7352915a0c984f
Marcos Sakurada <msakurad@c1r2p4.42sp.org.br>
2023-05-30 17:20:52 -0300

    feat: lstmap, Renan helped. Bonus concluded

--- srcs_others/ft_lstmap_bonus.c
+++ srcs_others/ft_lstmap_bonus.c
@@ -6,37 +6,27 @@
 /*   By: msakurad <msakurad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 23:31:40 by msakurad          #+#    #+#             */
-/*   Updated: 2023/05/30 16:01:20 by msakurad         ###   ########.fr       */
+/*   Updated: 2023/05/30 14:31:12 by msakurad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	check_error(t_list *lst, t_list *head, void (*del)(void *))
-{
-	if (lst == NULL)
-	{
-		del(lst->content);
-		ft_lstclear(&head, del);
-	}
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
-	t_list	*head;
+	t_list	*tmp;
 
+	newlst = NULL;
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	newlst = ft_lstnew(f(lst->content));
-	head = newlst;
-	check_error(newlst, head, del);
-	while (lst->next)
+	while (lst)
 	{
+		ft_lstadd_front(&newlst, ft_lstnew(f(lst->content)));
+		tmp = lst;
 		lst = lst->next;
-		newlst->next = ft_lstnew(f(lst->content));
-		check_error(newlst->next, head, del);
-		newlst = newlst->next;
+		ft_lstdelone(tmp, del);
+		free(tmp);
 	}
-	return (head);
+	return (newlst);
 }
